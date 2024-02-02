91056b49142871836ba25505058ab3a634f1c46c
orange-panther <katharinaeinzi@gmail.com>
2024-02-02 09:39:28 +0100

    Initial commit

--- starter/TrainSwitching.Logic/SwitchingOperationParser.cs
+++ starter/TrainSwitching.Logic/SwitchingOperationParser.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using System.Globalization;
+
 namespace TrainSwitching.Logic;
 
 public static class SwitchingOperationParser
@@ -9,7 +12,65 @@ public static class SwitchingOperationParser
     /// <returns>The parsed switching operation</returns>
     public static SwitchingOperation Parse(string inputLine)
     {
-        // TODO: Implement this method
-        throw new NotImplementedException();
+        SwitchingOperation myOperation = new();
+        int countTo = inputLine.IndexOf(',');
+        for (int i = 0; i <= countTo; i++)
+        {
+            if (int.TryParse(inputLine[i].ToString(), out int number))
+            {
+                if (int.TryParse(inputLine[i + 1].ToString(), out int number2))
+                {
+                    myOperation.TrackNumber = int.Parse(number.ToString() + number2.ToString());
+                }
+                else
+                {
+                    myOperation.TrackNumber = number;
+                    
+                }
+            }
+            if (i == countTo)
+            {
+                char operation = inputLine[i + 2];
+
+                myOperation.OperationType = operation switch
+                {
+                    'a' => Constants.OPERATION_ADD,
+                    'r' => Constants.OPERATION_REMOVE,
+                    't' => Constants.OPERATION_TRAIN_LEAVE,
+                    _ => throw new InvalidOperationException("This operation is not valid."),
+                };
+            }
+        }
+        if (inputLine.Contains("East"))
+        {
+            myOperation.Direction = Constants.DIRECTION_EAST;
+        }
+        else
+        {
+            myOperation.Direction = Constants.DIRECTION_WEST;
+        }
+
+        switch (myOperation.OperationType)
+        {
+            case Constants.OPERATION_ADD:
+                char firstChar = inputLine[inputLine.IndexOf("add") + 4];
+                myOperation.WagonType = firstChar switch
+                {
+                    'P' => Constants.WAGON_TYPE_PASSENGER,
+                    'L' => Constants.WAGON_TYPE_LOCOMOTIVE,
+                    'F' => Constants.WAGON_TYPE_FREIGHT,
+                    'C' => Constants.WAGON_TYPE_CAR_TRANSPORT,
+                    _ => throw new InvalidOperationException("This wagon type is not valid."),
+                };
+                break;
+            case Constants.OPERATION_REMOVE:
+                myOperation.NumberOfWagons = int.Parse(inputLine[inputLine.IndexOf("remove") + 7].ToString());
+                break;
+            default:
+                break;
+
+        }
+        return myOperation;
+
     }
 }
